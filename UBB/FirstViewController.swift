ba765a4d3ecd9ae352ad0cd0666a9286c0029bd6
Sébastien Gaya <somniia.99@gmail.com>
2017-11-08 14:30:43 +0100

    Initial Commit

---
+++ UBB/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  UBB
+//
+//  Created by Sébastien Gaya on 08/11/2017.
+//  Copyright © 2017 Sébastien Gaya. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
