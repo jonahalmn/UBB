95a399c7ab4f808799ebcfeb37bdf3998f3519c2
Sébastien Gaya <somniia.99@gmail.com>
2017-11-10 18:58:07 +0100

    Added Loading screen to NewsTableViewController

--- UBB/Controller/NewsTableViewController.swift
+++ UBB/Controller/NewsTableViewController.swift
@@ -11,9 +11,21 @@ import UIKit
 class NewsTableViewController: UITableViewController {
     
     var news = [News]()
+    
+    /// View which contains the loading text and the spinner
+    let loadingView = UIView()
+    
+    /// Spinner shown during load the TableView
+    let spinner = UIActivityIndicatorView()
+    
+    /// Text shown during load the TableView
+    let loadingLabel = UILabel()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setLoadingScreen()
         
         let newsLoadedName = Notification.Name(rawValue: "NewsLoaded")
         NotificationCenter.default.addObserver(self, selector: #selector(newsLoaded), name: newsLoadedName, object: nil)
@@ -112,6 +124,8 @@ class NewsTableViewController: UITableViewController {
     
     @objc func newsLoaded() {
         self.tableView.reloadData()
+        self.tableView.separatorStyle = .singleLine
+        removeLoadingScreen()
     }
     
     @objc func photoLoaded(notification: Notification) {
@@ -125,13 +139,64 @@ class NewsTableViewController: UITableViewController {
         }
     }
     
+    @IBAction func didTapRefreshButton(_ sender: UIBarButtonItem) {
+        self.loadNews()
+    }
+    
+    
     private func loadNews() {
+        self.news = [News]()
+        self.tableView.reloadData()
+        setLoadingScreen()
         NewsManager.shared.get { (news) in
             self.news = news
             let name = Notification.Name(rawValue: "NewsLoaded")
             let notification = Notification(name: name)
             NotificationCenter.default.post(notification)
         }
+    }
+    
+    // Set the activity indicator into the main view
+    private func setLoadingScreen() {
+        
+        self.tableView.separatorStyle = .none
+        
+        // Sets the view which contains the loading text and the spinner
+        let width: CGFloat = 120
+        let height: CGFloat = 30
+        let x = (tableView.frame.width / 2) - (width / 2)
+        let y = (tableView.frame.height / 2) - (height / 2) - (navigationController?.navigationBar.frame.height)!
+        loadingView.frame = CGRect(x: x, y: y, width: width, height: height)
+        
+        // Sets loading text
+        loadingLabel.textColor = .gray
+        loadingLabel.textAlignment = .center
+        loadingLabel.text = "Loading..."
+        loadingLabel.frame = CGRect(x: 0, y: 0, width: 140, height: 30)
+        
+        // Sets spinner
+        spinner.activityIndicatorViewStyle = .gray
+        spinner.frame = CGRect(x: 0, y: 0, width: 30, height: 30)
+        spinner.isHidden = false
+        loadingLabel.isHidden = false
+        spinner.startAnimating()
+        
+        // Adds text and spinner to the view
+        loadingView.addSubview(spinner)
+        loadingView.addSubview(loadingLabel)
+        
+        tableView.addSubview(loadingView)
+        
+    }
+    
+    // Remove the activity indicator from the main view
+    private func removeLoadingScreen() {
+        
+        // Hides and stops the text and the spinner
+        spinner.stopAnimating()
+        spinner.isHidden = true
+        loadingLabel.isHidden = true
+        
     }
 
 }
