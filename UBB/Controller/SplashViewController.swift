2f09276deba0b8e66ec3bdf14eb44b0b815ab40b
jonahalmn <jonah.allemonne@icloud.com>
2017-11-10 18:54:43 +0100

    splash

---
+++ UBB/Controller/SplashViewController.swift
@@ -0,0 +1,39 @@
+//
+//  SplashViewController.swift
+//  UBB
+//
+//  Created by Jonah Alle Monne on 10/11/2017.
+//  Copyright © 2017 Sébastien Zaza. All rights reserved.
+//
+
+import UIKit
+
+class SplashViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        DispatchQueue.main.asyncAfter(deadline: .now() + 4.5) {
+            let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+            let newViewController = storyBoard.instantiateViewController(withIdentifier: "NewsTableViewController")
+            self.present(newViewController, animated: true, completion: nil)
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
