92471a72ba2a8a4402bbf6ffb5574a7f45c97e3e
Sébastien Gaya <somniia.99@gmail.com>
2018-01-30 08:19:26 +0100

    Navigation Bar gets in & out

---
+++ UBB/Controller/AnonymousViewController.swift
@@ -0,0 +1,42 @@
+//
+//  AnonymousViewController.swift
+//  UBB
+//
+//  Created by Sébastien Gaya on 30/01/2018.
+//  Copyright © 2018 Sébastien Gaya. All rights reserved.
+//
+
+import UIKit
+
+class AnonymousViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.isNavigationBarHidden = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        self.navigationController?.isNavigationBarHidden = false
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
